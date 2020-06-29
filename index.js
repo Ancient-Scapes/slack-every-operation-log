8689c776e374e77b863ac07259e960f6dee8ebdb
Ancient-Scapes <ryu.8224@gmail.com>
2020-06-30 04:16:04 +0900

    fix: @slack/web-api → @slack/bolt / feat sample of conversations API

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 require('dotenv').config()
 
-const { WebClient } = require('@slack/web-api');
-const token = process.env.SLACK_BOT_TOKEN;
-const web = new WebClient(token);
+const { App } = require('@slack/bolt')
+const token = process.env.SLACK_BOT_TOKEN
+const app = new App({
+  token,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+})
 
-const postMessage = async () => {
-  const result = await web.chat.postMessage({
-    text: 'こんにちは',
-    channel: `#${process.env.POST_CHANNEL}`,
-  });
 
-  console.log(`Successfully send message ${result.ts}`);
+async function joinChannel(id) {
+  try {
+    await app.client.conversations.join({
+      token,
+      channel: id,
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
-postMessage()
+let conversationHistory
+
+async function fetchHistory(id) {
+  try {
+    const result = await app.client.conversations.history({
+      token,
+      channel: id
+    })
+
+    conversationHistory = result.messages
+    console.log(result)
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+(async () => {
+  await app.start(process.env.PORT || 3000)
+  console.log("⚡️ Bolt app is running!")
+  fetchHistory(process.env.FETCH_CHANNEL)
+})()
